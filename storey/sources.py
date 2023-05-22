5c90219be1dd77141883f0087c7c3c2b342c9c93
Gal Topper <galt@iguazio.com>
2023-05-22 20:11:00 +0800

    Fix

--- storey/sources.py
+++ storey/sources.py
@@ -535,7 +535,7 @@ async def _commit_handled_events(outstanding_offsets_by_qualified_shard, committ
                     for referrer in gc.get_referrers(event):
                         print(
                             f"!!!     referrer (id={id(referrer)}) is of type {type(referrer)} from module "
-                            f"{referrer.__module__}, with methods {dir(referrer)}"
+                            f"{type(referrer).__module__}, with methods {dir(referrer)}"
                         )
                     print(f"{gc.get_referrers()}")
                     all_offsets_handled = False
