7e4c8ef7f436c23121801596a3ec8704f05333e3
Gal Topper <galt@iguazio.com>
2023-07-12 17:47:20 +0800

    collect gc

--- storey/sources.py
+++ storey/sources.py
@@ -543,7 +543,7 @@ async def _commit_handled_events(outstanding_offsets_by_qualified_shard, committ
         else:
             num_to_clear = 0
             last_handled_offset = None
-            # gc.collect()
+            gc.collect()
             # go over offsets in the qualified shard by arrival order until we reach an unhandled offset
             for offset in offsets:
                 if not offset.is_ready_to_commit():
