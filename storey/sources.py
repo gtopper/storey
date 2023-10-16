1ef3bb7d607863ad6f6d4a2561a5eaf8b2c03b33
Gal Topper <galt@iguazio.com>
2023-10-16 13:30:27 +0800

    Add committer print

--- storey/sources.py
+++ storey/sources.py
@@ -535,6 +535,10 @@ async def _commit_handled_events(outstanding_offsets_by_qualified_shard, committ
         print(f"111 _commit_handled_events: last_handled_offset={last_handled_offset}")
         if last_handled_offset is not None:
             path, shard_id = qualified_shard
+            print(
+                f"111 _commit_handled_events: calling committer with QualifiedOffset with path={path} "
+                f"shard_id={shard_id} last_handled_offset={last_handled_offset}"
+            )
             await committer(QualifiedOffset(path, shard_id, last_handled_offset))
             outstanding_offsets_by_qualified_shard[qualified_shard] = offsets[num_to_clear:]
     return all_offsets_handled
