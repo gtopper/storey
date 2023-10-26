d3b12a27018240f624063535e6422d762577a631
Gal Topper <galt@iguazio.com>
2023-10-26 19:29:50 +0800

    Fix

--- storey/sources.py
+++ storey/sources.py
@@ -305,6 +305,7 @@ class SyncEmitSource(Flow):
                 # Due to the last event not being garbage collected, we tolerate a single unhandled event
                 # TODO: Remove after transitioning to AsyncEmitSource, which would solve the underlying problem
                 can_block = num_offsets_not_handled <= 1
+                num_events_handled_without_commit = 0
             while not can_block:
                 try:
                     event = await loop.run_in_executor(None, self._q.get, True, self._max_wait_before_commit)
@@ -313,6 +314,7 @@ class SyncEmitSource(Flow):
                     pass
                 num_offsets_not_handled = await _commit_handled_events(self._outstanding_offsets, committer)
                 can_block = num_offsets_not_handled <= 1
+                num_events_handled_without_commit = 0
             if event is None:
                 event = await loop.run_in_executor(None, self._q.get)
             if committer and hasattr(event, "path") and hasattr(event, "shard_id") and hasattr(event, "offset"):
