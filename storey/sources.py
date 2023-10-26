f5a0dc3d5f383e46975d906ef8cc925ed5fd4853
Gal Topper <galt@iguazio.com>
2023-10-26 20:25:33 +0800

    Fixes

--- storey/sources.py
+++ storey/sources.py
@@ -294,35 +294,30 @@ class SyncEmitSource(Flow):
         loop = asyncio.get_running_loop()
         self._termination_future = loop.create_future()
         committer = None
-        num_events_handled_without_commit = 0
+        num_offsets_not_handled = 0
         if self._explicit_ack and hasattr(self.context, "platform") and hasattr(self.context.platform, "explicit_ack"):
             committer = self.context.platform.explicit_ack
         while True:
             event = None
-            can_block = False
-            if num_events_handled_without_commit >= self._max_events_before_commit:
-                num_offsets_not_handled = await _commit_handled_events(self._outstanding_offsets, committer)
+            if committer:
+                if num_offsets_not_handled >= self._max_events_before_commit:
+                    num_offsets_not_handled = await _commit_handled_events(self._outstanding_offsets, committer)
                 # Due to the last event not being garbage collected, we tolerate a single unhandled event
                 # TODO: Remove after transitioning to AsyncEmitSource, which would solve the underlying problem
-                can_block = num_offsets_not_handled <= 1
-                num_events_handled_without_commit = 0
-            if num_events_handled_without_commit > 0:
-                while not can_block:
+                while num_offsets_not_handled > 1:
                     try:
                         event = await loop.run_in_executor(None, self._q.get, True, self._max_wait_before_commit)
                         break
                     except queue.Empty:
                         pass
                     num_offsets_not_handled = await _commit_handled_events(self._outstanding_offsets, committer)
-                    can_block = num_offsets_not_handled <= 1
-                    num_events_handled_without_commit = 0
             if event is None:
                 event = await loop.run_in_executor(None, self._q.get)
             if committer and hasattr(event, "path") and hasattr(event, "shard_id") and hasattr(event, "offset"):
                 qualified_shard = (event.path, event.shard_id)
                 offsets = self._outstanding_offsets[qualified_shard]
                 offsets.append(_EventOffset(event))
-                num_events_handled_without_commit += 1
+                num_offsets_not_handled += 1
             try:
                 termination_result = await self._do_downstream(event)
                 if event is _termination_obj:
